99cedde6c1b96841f84669eaba4bb214c8408345
feDev <151403951+ichigosaurion@users.noreply.github.com>
2025-04-25 05:12:37 -0300

    Se cambio para que ignore dll y la carpeta de SFML
    cuando se haga pull a esto, se le deberá agregar los dll a la carpeta llamada dll, todos juntos.
    tambien, se hizo qeu ignore las carpetas de sfml y los dll al hacer commit, asi quees solo agregarlo una vez cuando se hace pull por primera vez

--- SFMLTemplate/SFMLTemplate.cpp
+++ SFMLTemplate/SFMLTemplate.cpp
@@ -2,31 +2,16 @@
 
 int main()
 {
-    // Crear una ventana de 800x600
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Hello World");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Plantilla SFML");
 
-    // Crear un círculo
-    sf::CircleShape shape(50);
-    shape.setFillColor(sf::Color::Green);
-
-    // Bucle principal de la ventana
     while (window.isOpen())
     {
-        // Comprobar los eventos
         sf::Event event;
         while (window.pollEvent(event))
-        {
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
 
-        // Limpiar la ventana
         window.clear();
-
-        // Dibujar el círculo
-        window.draw(shape);
-
-        // Mostrar lo que se ha dibujado
         window.display();
     }
 
